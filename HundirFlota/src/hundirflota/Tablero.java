f59cc2829a5d4207f8ed59526f5bc8d622ab669b
Alejandro0120 <73612152+Alejandro0120@users.noreply.github.com>
2022-02-23 20:59:09 +0100

    Tablero constructor
    Constructor tablero

--- HundirFlota/src/hundirflota/Tablero.java
+++ HundirFlota/src/hundirflota/Tablero.java
@@ -10,5 +10,67 @@ package hundirflota;
  * @author Usuario 1
  */
 public class Tablero {
+    private String nombre;
+    private int tamanio;
+
+    public Tablero(String nombre, int tamanio) {
+        this.nombre = nombre;
+        this.tamanio = tamanio;
+    }
     
+    public void mostrarTablero() {
+        
+        //cabecera
+        System.out.printf("%4s"," ");
+        for (int i = 0; i < (tamanio); i++) {
+     
+            System.out.printf("%d ",i);
+            if(i==(tamanio)-1){
+            
+           System.out.printf("\n %3s"," ");
+            }
+            
+        }
+         for (int i = 0; i < (tamanio); i++) {
+         System.out.printf("%s ","-");
+          if(i==(tamanio)-1){
+            
+           System.out.printf("\n");
+            }
+         
+         }
+
+        //cuerpo
+        for (int i = 0; i <tamanio; i++) {
+            System.out.printf( "%d %s ",i,"|");
+            for (int j = 0; j < tamanio; j++) {
+                System.out.println("*");
+            }
+            System.out.printf( "%s%d","|",i);
+            System.out.println(" ");
+        }
+        
+        //cierre
+        System.out.printf("%4s", " ");
+      
+         for (int i = 0; i < (tamanio); i++) {
+         System.out.printf("%s ","-");
+          if(i==(tamanio)-1){
+            
+           System.out.printf("\n");
+            }
+         
+         }
+         System.out.printf(" %3s"," ");
+           for (int i = 0; i < (tamanio); i++) {
+     
+            System.out.printf("%d ",i);
+           
+        }
+         
+          System.out.println("");
+        
+        
+        System.out.println("Jugador/a: "+nombre);
+    }
 }
